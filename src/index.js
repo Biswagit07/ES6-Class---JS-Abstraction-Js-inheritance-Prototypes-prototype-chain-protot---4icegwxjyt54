ea8ec441f9386218403ae33d3cc92f5e0bc48233
Biswa <biswajitrout8093@gmail.com>
2024-01-26 01:17:21 +0530

    your message

--- src/index.js
+++ src/index.js
@@ -1,4 +1,25 @@
 //Write your code here
+class Api {
+    #secure:
+
+
+constructor(url, method = 'GET') {
+    this.url = url;
+    this.method = method;
+    this.#secure = url.startsWith('https');
+}
+
+isSecure() {
+    return this.#secure;
+}
+
+updateUrl(newUrl) {
+    this.url = newUrl;
+    this.#secure = newUrl.startsWith(https);
+}
+}
+
+
 
 
 
